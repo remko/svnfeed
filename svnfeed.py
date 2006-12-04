50373e728d8655c3fa4ea86dd27184d814defa14
Remko Tronçon <dev@el-tramo.be>
2006-12-04 10:19:37 +0000

    Auto-detection of '--limit' option
    git-svn-id: file:///home/remko/svn/svnfeed/trunk@2 c1ecb42d-c423-0410-9724-c3bf9a1a7caf

--- svnfeed.py
+++ svnfeed.py
@@ -1,12 +1,41 @@
 #!/usr/bin/env python
 
-import os, xml, cgi, sys
+import os, xml, cgi, sys, optparse, re
 from xml.dom import minidom
 
 
 ################################################################################
+# Module information
+################################################################################
+
+__author__ = 'Remko Troncon'
+__version__ = '0.1-dev'
+__copyright__ = """
+    Copyright (C) 2006  Remko Troncon
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    """
+
+################################################################################
 # Constants
 ################################################################################
+
+PROGRAM_NAME = 'SVNFeed'
+PROGRAM_URI = 'http://el-tramo.be/software/svnfeed'
+PROGRAM_USAGESTRING = "usage: %prog [options] repository-uri"
+PROGRAM_VERSIONSTRING = PROGRAM_NAME + ' ' + __version__ + '\nWritten by ' + __author__ + '\n' + 'For more information, please visit ' + PROGRAM_URI
 
 ATOM_NS = 'http://www.w3.org/2005/Atom'
 
@@ -32,9 +61,26 @@ class SVNLogEntry :
 	self.msg = ''
 	
 def svn_entries(repo, nb_entries) :
+    # Initialize SVN features
+    use_limit = False
+
+    # Determine the SVN version
+    stream = os.popen('svn --version')
+    if not stream : 
+	return None
+    version_string = stream.read()
+    m = re.match(".*version (?P<major>\d+)\.(?P<minor>\d+).*", version_string)
+    if m != None :
+	version = int(m.group('major'))*100+int(m.group('minor'))
+	if version >= 102 :
+	    use_limit = True
+
     # Read the text
+    svn_command = 'svn log --xml '
+    if use_limit and nb_entries > 0 :
+	svn_command += '--limit ' + str(nb_entries) + ' '
     stream = os.popen('svn log --xml ' + repo)
-    if stream == None :
+    if not stream :
 	return None
 
     # Parse the document
@@ -121,10 +167,11 @@ def generate_feed(title, repository, max_entries, uri) :
 	entry_node.appendChild(author_node)
 
 	# Alternate link
-	link_node = doc.createElement('link')
-	link_node.setAttribute('rel', 'alternate')
-	link_node.setAttribute('href', uri % { 'revision' : entry.revision })
-	entry_node.appendChild(link_node)
+	if uri :
+	    link_node = doc.createElement('link')
+	    link_node.setAttribute('rel', 'alternate')
+	    link_node.setAttribute('href', uri % { 'revision' : entry.revision })
+	    entry_node.appendChild(link_node)
 
 	# Content
 	content_node = doc.createElement('content')
@@ -139,16 +186,26 @@ def generate_feed(title, repository, max_entries, uri) :
 
 
 def main() :
-    repository = 'http://svn.jivesoftware.org/svn/repos/wildfire/trunk'
-    title = 'Wildfire SVN repository'
-    max_entries = 10
-    filename = '/var/www/feeds/wildfire-svn.xml'
-
-    feed = generate_feed(title = title, repository = repository, max_entries = max_entries, uri = '')
-    if feed :
-	out = open(filename, 'w')
+    # Parse the arguments
+    parser = optparse.OptionParser(usage=PROGRAM_USAGESTRING, version=PROGRAM_VERSIONSTRING)
+    parser.add_option('-t', '--title', metavar='TITLE', help='The title of the target feed', default='Feed for URI')
+    parser.add_option('-f', '--file', metavar='FILE', help='File to output the feed to (stdout if omitted)')
+    parser.add_option('', '--max-entries', metavar='ENTRIES', help='The maximum number of entries in the feed. Default: %default', type='int', default=10)
+    parser.add_option('-u', '--uri', metavar='URI', help='Link to the web interface (e.g. \'http://example.com/%(revision)s\')')
+    (options, args) = parser.parse_args()
+    
+    # Get the repository URI
+    if len(args) != 1 :
+	sys.exit('Repository URI missing')
+    repository = args[0]
+    
+    feed = generate_feed(title = options.title, repository = repository, max_entries = options.max_entries, uri = options.uri)
+    if feed and options.file :
+	out = open(options.file, 'w')
 	out.write(feed)
 	out.close()
+    else :
+	print feed
 
 if __name__ == "__main__" :
     main()
