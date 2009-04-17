100d847d48e152b449e243050db4e757ae0818de
Remko Tronçon <git@el-tramo.be>
2009-04-17 18:56:43 +0200

    Catch expat errors. Retabbing.

--- svnfeed.py
+++ svnfeed.py
@@ -45,21 +45,21 @@ ATOM_NS = 'http://www.w3.org/2005/Atom'
 
 def node_text(node) :
     for child in node.childNodes :
-	if child.nodeType == xml.dom.Node.TEXT_NODE :
-	    return child.data
-	
-	
+        if child.nodeType == xml.dom.Node.TEXT_NODE :
+            return child.data
+        
+        
 ################################################################################
 # Subversion functions
 ################################################################################
-	
+        
 class SVNLogEntry :
     def __init__(self) :
-	self.revision = -1
-	self.author = ''
-	self.date = ''
-	self.msg = ''
-	
+        self.revision = -1
+        self.author = ''
+        self.date = ''
+        self.msg = ''
+        
 def svn_entries(repo, nb_entries) :
     # Initialize SVN features
     use_limit = False
@@ -67,47 +67,50 @@ def svn_entries(repo, nb_entries) :
     # Determine the SVN version
     stream = os.popen('svn --version')
     if not stream : 
-	return None
+        return None
     version_string = stream.read()
     m = re.match(".*version (?P<major>\d+)\.(?P<minor>\d+).*", version_string)
     if m != None :
-	version = int(m.group('major'))*100+int(m.group('minor'))
-	if version >= 102 :
-	    use_limit = True
+        version = int(m.group('major'))*100+int(m.group('minor'))
+        if version >= 102 :
+            use_limit = True
 
     # Read the text
     svn_command = 'svn log --xml '
     if use_limit and nb_entries > 0 :
-	svn_command += '--limit ' + str(nb_entries) + ' '
+        svn_command += '--limit ' + str(nb_entries) + ' '
     stream = os.popen('svn log --xml ' + repo)
     if not stream :
-	return None
+        return None
 
     # Parse the document
     entries = []
-    doc = minidom.parseString(stream.read())
+    try :
+            doc = minidom.parseString(stream.read())
+    except xml.parsers.expat.ExpatError :
+            return None
     doc.normalize()
     for log_entry in doc.getElementsByTagName('logentry') :
-	entry = SVNLogEntry()
-	entry.revision = int(log_entry.getAttribute('revision'))
-	for property_node in log_entry.childNodes :
-	    if property_node.nodeType != xml.dom.Node.ELEMENT_NODE :
-		continue
+        entry = SVNLogEntry()
+        entry.revision = int(log_entry.getAttribute('revision'))
+        for property_node in log_entry.childNodes :
+            if property_node.nodeType != xml.dom.Node.ELEMENT_NODE :
+                continue
 
-	    value = node_text(property_node)
-	    if property_node.tagName == "author" :
-		entry.author = value
-	    elif property_node.tagName == "date" :
-		entry.date = value
-	    elif property_node.tagName == "msg" :
-		entry.msg = value
+            value = node_text(property_node)
+            if property_node.tagName == "author" :
+                entry.author = value
+            elif property_node.tagName == "date" :
+                entry.date = value
+            elif property_node.tagName == "msg" :
+                entry.msg = value
 
-	entries.append(entry)
-	
+        entries.append(entry)
+        
     # Remove the oldest entries
     entries.sort(lambda x, y : cmp(y.revision, x.revision))
     if nb_entries > 0 :
-	entries = entries[:nb_entries]
+        entries = entries[:nb_entries]
 
     return entries
 
@@ -119,6 +122,8 @@ def svn_entries(repo, nb_entries) :
 def generate_feed(title, repository, max_entries, uri) :
     # Retrieve the entries
     entries = svn_entries(repository, max_entries)
+    if entries == None :
+        return None
 
     # Create an atom feed
     doc = xml.dom.minidom.getDOMImplementation().createDocument(ATOM_NS, 'feed', None)
@@ -126,9 +131,9 @@ def generate_feed(title, repository, max_entries, uri) :
 
     # Title
     if title :
-	title_node = doc.createElement('title')
-	title_node.appendChild(doc.createTextNode(title))
-	doc.documentElement.appendChild(title_node)
+        title_node = doc.createElement('title')
+        title_node.appendChild(doc.createTextNode(title))
+        doc.documentElement.appendChild(title_node)
 
     # ID
     id = doc.createElement('id')
@@ -149,47 +154,47 @@ def generate_feed(title, repository, max_entries, uri) :
     
     # Entries
     for entry in entries :
-    	if not entry.msg :
-		entry.msg = '(Empty commit message)'
+        if not entry.msg :
+                entry.msg = '(Empty commit message)'
 
-	entry_node = doc.createElement('entry')
-	
-	# ID
-	id_node = doc.createElement('id')
-	id_node.appendChild(doc.createTextNode(repository + '#' + str(entry.revision)))
-	entry_node.appendChild(id_node)
+        entry_node = doc.createElement('entry')
+        
+        # ID
+        id_node = doc.createElement('id')
+        id_node.appendChild(doc.createTextNode(repository + '#' + str(entry.revision)))
+        entry_node.appendChild(id_node)
 
-	# Updated
-	updated_node = doc.createElement('updated')
-	updated_node.appendChild(doc.createTextNode(entry.date))
-	entry_node.appendChild(updated_node)
+        # Updated
+        updated_node = doc.createElement('updated')
+        updated_node.appendChild(doc.createTextNode(entry.date))
+        entry_node.appendChild(updated_node)
 
-	# Title
-	title_node = doc.createElement('title')
-	title_node.appendChild(doc.createTextNode(entry.msg))
-	entry_node.appendChild(title_node)
+        # Title
+        title_node = doc.createElement('title')
+        title_node.appendChild(doc.createTextNode(entry.msg))
+        entry_node.appendChild(title_node)
 
-	# Authors
-	author_node = doc.createElement('author')
-	name_node = doc.createElement('name')
-	name_node.appendChild(doc.createTextNode(entry.author))
-	author_node.appendChild(name_node)
-	entry_node.appendChild(author_node)
+        # Authors
+        author_node = doc.createElement('author')
+        name_node = doc.createElement('name')
+        name_node.appendChild(doc.createTextNode(entry.author))
+        author_node.appendChild(name_node)
+        entry_node.appendChild(author_node)
 
-	# Alternate link
-	if uri :
-	    link_node = doc.createElement('link')
-	    link_node.setAttribute('rel', 'alternate')
-	    link_node.setAttribute('href', uri % { 'revision' : entry.revision })
-	    entry_node.appendChild(link_node)
+        # Alternate link
+        if uri :
+            link_node = doc.createElement('link')
+            link_node.setAttribute('rel', 'alternate')
+            link_node.setAttribute('href', uri % { 'revision' : entry.revision })
+            entry_node.appendChild(link_node)
 
-	# Content
-	content_node = doc.createElement('content')
-	content_node.setAttribute('type', 'text')
-	content_node.appendChild(doc.createTextNode(entry.msg))
-	entry_node.appendChild(content_node)
-	
-	doc.documentElement.appendChild(entry_node)
+        # Content
+        content_node = doc.createElement('content')
+        content_node.setAttribute('type', 'text')
+        content_node.appendChild(doc.createTextNode(entry.msg))
+        entry_node.appendChild(content_node)
+        
+        doc.documentElement.appendChild(entry_node)
 
     #return doc.toprettyxml()
     return doc.toxml('utf-8')
@@ -206,16 +211,16 @@ def main() :
     
     # Get the repository URI
     if len(args) != 1 :
-	sys.exit('Repository URI missing')
+        sys.exit('Repository URI missing')
     repository = args[0]
     
     feed = generate_feed(title = options.title, repository = repository, max_entries = options.max_entries, uri = options.uri)
     if feed and options.file :
-	out = open(options.file, 'w')
-	out.write(feed)
-	out.close()
+        out = open(options.file, 'w')
+        out.write(feed)
+        out.close()
     else :
-	print feed
+        print feed
 
 if __name__ == "__main__" :
     main()
